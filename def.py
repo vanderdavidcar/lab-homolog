e63ff5f5b3c7ba89d25a0c4e4798642b2667276c
Vanderson <vander_davidcar@yahoo.com.br>
2022-12-16 20:48:13 -0300

    update regex pattern

--- def.py
+++
@@ -1,8 +0,0 @@
-def sum_the_numbers (*args):              # define our function
-  total = 0                               # set initial total to zero
-  for num in args:                        # loop through each value
-    total += num                          # add each value to the total
-    return total                          # return the summed total
-  
-summation = sum_the_numbers(5, 10)        # execute our function
-print(summation)                          # print the total
