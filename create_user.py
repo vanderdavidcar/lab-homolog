6cd3df577ba94e58d370bf654df3b1da38843b25
Vanderson <vander_davidcar@yahoo.com.br>
2022-12-18 15:14:40 -0300

    module create_users.py

--- create_user.py
+++ create_user.py
@@ -1,19 +1,23 @@
 from netmiko import ConnectHandler
 import net_conn
+from get_user_priv_lvl import get_user_priv_lvl
 
 with open("devices") as f:
     lines = f.read().splitlines()
 print(lines)
 
-for devices in lines:
-    iosv = net_conn.netmiko_connection(devices)
-    print("#"*78)
-    print("Connecting to " + str(devices))
-    net_connect = ConnectHandler(**iosv)
+def create_users(lines):
+    for devices in lines:
+        iosv = net_conn.netmiko_connection(devices)
+        print("#"*78)
+        print("Connecting to " + str(devices))
+        net_connect = ConnectHandler(**iosv)
 
-    config_cmd = net_connect.send_config_from_file(f"config_cmd")
-    save_cmd = net_connect.send_command(f"save config")
-    save_cmd = net_connect.send_command(f"y")
-    print(config_cmd)
-    show_cmd = net_connect.send_command(f"sh run | in username")
-    print(show_cmd)
+        config_cmd = net_connect.send_config_from_file(f"config_cmd")
+        save_cmd = net_connect.send_command(f"save config")
+        save_cmd = net_connect.send_command(f"y")
+        print(config_cmd)
+        show_cmd = net_connect.send_command(f"sh run | in username")
+        print(show_cmd)
+
+create_users(lines)
