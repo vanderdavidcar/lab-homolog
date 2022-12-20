b73cd887cfd59e6c7ed369b01071af775a9540b6
Vanderson <vander_davidcar@yahoo.com.br>
2022-12-19 21:54:14 -0300

    update function create_scripts

--- create_scripts.py
+++ create_scripts.py
@@ -7,17 +7,19 @@ import yaml
 with open("devices")as f:
     devices_list = f.read().splitlines()
 
-#Load data from YAML file into Python dictionary
-config = yaml.full_load(open('./auth_vars.yml'))
+def create_scripts():
+    #Load data from YAML file into Python dictionary
+    config = yaml.full_load(open('./auth_vars.yml'))
 
-#Load Jinja2 template
-env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+    #Load Jinja2 template
+    env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 
-for i in devices_list:
-    template = env.get_template(f'default_template.j2')
-    output = template.render(config)
-    scriptFile = open(f'config_default.cfg', "w+")
-    scriptFile.write(output)
-    print(f'Created script FileName config_default.cfg')
-    print(output)
-    
+    for i in devices_list:
+        template = env.get_template(f'default_template.j2')
+        output = template.render(config)
+        scriptFile = open(f'config_default.cfg', "w+")
+        scriptFile.write(output)
+        print(f'Created script FileName config_default.cfg')
+        print(output)
+
+create_scripts()
